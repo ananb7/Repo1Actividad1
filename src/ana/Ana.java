6a625187de9da28f2a22f987901dc35f3833d52b
Ana Nuñez <anaym10@gmail.com>
2023-01-28 00:01:18 +0100

    añadir Ana.java

---
+++ src/ana/Ana.java
@@ -0,0 +1,18 @@
+package ana;
+
+public class Ana {
+
+	public static void main(String[] args) {
+		miNombre();
+	}
+		
+	public static void miNombre() {
+		System.out.println("Hola, mi nombre es Ana. ");
+	}
+	
+	
+	
+		
+}
+
+
