0730fcf9bb3f49db454c2bac891f38cbd4ccab2d
Javier Nuñez <jnb89pr@gmail.com>
2023-01-28 00:10:26 +0100

    Subir clase Javi

---
+++ src/Javier/Javier.java
@@ -0,0 +1,14 @@
+package Javier;
+
+public class Javier {
+
+	public static void main(String[] args) {
+		String nombre = "Javier";
+		
+		System.out.println(darNombre(nombre));
+
+	}
+		public static String darNombre(String a) {
+			return ("Hola soy "+a);
+		}
+}
