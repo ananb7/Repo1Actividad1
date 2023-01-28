049346aa836340405587db269f6bfc7580064b95
NebilSMTH <nabilmakraneaguilar18@gmail.com>
2023-01-28 16:21:44 +0100

    Añadido Nabil

---
+++ src/Nabil/src/nabilpackage/Nabil.java
@@ -0,0 +1,14 @@
+package nabilpackage;
+
+public class Nabil {
+
+	public static void main(String[] args) {
+		nombre();
+
+	}
+	public static void nombre() {
+		System.out.println("Hola, mi nombre es Nabil. ");
+	}
+	
+
+}
