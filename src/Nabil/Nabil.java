80d6f5ac57b0092a56e9b62e3cb8d8588581a83a
Ana Nuñez <anaym10@gmail.com>
2023-02-05 13:35:50 +0100

    actualización comidas2 nevil

--- src/Nabil/Nabil.java
+++ src/Nabil/Nabil.java
@@ -10,7 +10,7 @@ public class Nabil {
 		System.out.println("Hola, mi nombre es Nabil . ");
 		System.out.println("Mis tres hobbies son: videojuegos, deporte y ver series. ");
 		System.out.println("-------------------");
-		System.out.println("Mis tres comidas favoritas son: ");
+		System.out.println(" Mis tres comidas favoritas son: ");
 		System.out.println(" - croquetas. ");
 		System.out.println(" - tortilla de patata. ");
 		System.out.println(" - salmón ahumado. ");
